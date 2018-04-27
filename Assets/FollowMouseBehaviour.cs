a339c73950d2f5b543a20fce38a1d6dfefa9d4fe
Ivars Bergs <ivarsbergs@Ivarss-MacBook-Pro.local>
2018-04-28 00:05:39 +0300

    Add arm mockup with character hinge

---
+++ Assets/FollowMouseBehaviour.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowMouseBehaviour : MonoBehaviour
+{
+
+    void Start()
+    {
+    }
+
+    void Update()
+    {
+        Vector3 temp = Input.mousePosition;
+        temp.z = 10f; // Set this to be the distance you want the object to be placed in front of the camera.
+        this.transform.position = Camera.main.ScreenToWorldPoint(temp);
+    }
+}
