473a25783bea978be31d419f36cadb45a54e61f5
Sacristan <girts.kesteris@gmail.com>
2018-04-28 23:54:46 +0300

    add arruw

--- Assets/InfoArrow/InfoArrow.cs
+++ Assets/InfoArrow/InfoArrow.cs
@@ -1,11 +1,21 @@
-﻿using System.Collections;
+﻿//#define DEBUG_INFO_ARROW
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using Sacristan.Utils;
 
 public class InfoArrow : Singleton<InfoArrow>
 {
-    [SerializeField] private Transform target;
+    [SerializeField] private Transform meshRoot;
+    [SerializeField] private float intensity = 0.5f;
+
+#if DEBUG_INFO_ARROW
+    [SerializeField]
+#endif
+    private Transform target;
+
+
+    private bool isEnabled = false;
 
     public Transform Target
     {
@@ -13,17 +23,38 @@ public class InfoArrow : Singleton<InfoArrow>
         set { target = value; }
     }
 
-    private IEnumerator Start()
+    protected override void Awake()
     {
-        while (true)
+        base.Awake();
+        Enable(false);
+    }
+
+#if DEBUG_INFO_ARROW
+    private void Start()
+    {
+        Enable(true);
+    }
+#endif
+
+    private IEnumerator ArrowRoutine()
+    {
+        while (isEnabled)
         {
             if (target != null)
             {
                 transform.LookAt(target);
+                meshRoot.transform.localPosition = Vector3.forward * Mathf.PingPong(Time.time, intensity);
             }
 
             yield return null;
         }
+    }
+
+    public void Enable(bool flag)
+    {
+        isEnabled = flag;
+        meshRoot.gameObject.SetActive(isEnabled);
+        if (isEnabled) StartCoroutine(ArrowRoutine());
     }
 
 }
