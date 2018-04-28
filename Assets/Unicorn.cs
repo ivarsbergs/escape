cbf0d727e3a25e4a993e36b2bc6521e6fa481c24
PETER-PC\peter <peterislauris@gmail.com>
2018-04-28 23:51:37 +0300

    Final animation

---
+++ Assets/Unicorn.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Unicorn : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void CallEnd()
+    {
+        Debug.Log("Call end");
+        EndLevelControl.Instance.StartFadeToWhite();
+    }
+}
