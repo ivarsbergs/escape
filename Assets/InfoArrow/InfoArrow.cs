c970646332a940bf1b329e39a73af7ef24527eaa
Sacristan <girts.kesteris@gmail.com>
2018-04-28 23:14:14 +0300

    add info arrow base

---
+++ Assets/InfoArrow/InfoArrow.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Sacristan.Utils;
+
+public class InfoArrow : Singleton<InfoArrow>
+{
+    [SerializeField] private Transform target;
+
+    public Transform Target
+    {
+        get { return target; }
+        set { target = value; }
+    }
+
+    private IEnumerator Start()
+    {
+        while (true)
+        {
+            if (target != null)
+            {
+                transform.LookAt(target);
+            }
+
+            yield return null;
+        }
+    }
+
+}
