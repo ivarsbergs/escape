8806e70a2829ed99601b3f0e6cb2289413613e3f
Sacristan <girts.kesteris@gmail.com>
2018-04-28 13:03:19 +0300

    add blud

---
+++ Assets/_BLUD/ParticleSplatter/Scripts/ParticleLauncher.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ParticleLauncher : ParticleLauncherBase
+{
+
+    void Update()
+    {
+        if (Input.GetButton("Fire1"))
+        {
+            ShootParticle();
+        }
+
+    }
+}
