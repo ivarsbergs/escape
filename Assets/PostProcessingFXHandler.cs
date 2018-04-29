3657887aa8c7377bfa3f3ccef39ca149313adb02
Sacristan <girts.kesteris@gmail.com>
2018-04-29 12:51:08 +0300

    vignette fx wip

---
+++ Assets/PostProcessingFXHandler.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.PostProcessing;
+
+public class PostProcessingFXHandler : MonoBehaviour
+{
+
+    [SerializeField] private float fadeTime = 1f;
+
+    private PostProcessingBehaviour _postProcessingBehaviour;
+    private VignetteModel.Settings _vignetteSettings;
+
+    private void Start()
+    {
+        _postProcessingBehaviour = GetComponent<PostProcessingBehaviour>();
+        UpdateEnd(0f);
+        StartCoroutine(EndRoutine());
+    }
+
+    private IEnumerator EndRoutine()
+    {
+        float t = 0f;
+
+        PostProcessingProfile postProcessingProfile = _postProcessingBehaviour.profile;
+        _vignetteSettings = postProcessingProfile.vignette.settings;
+
+        while (t <= 1f)
+        {
+            t += Time.deltaTime / fadeTime;
+            UpdateEnd(t);
+            yield return null;
+        }
+    }
+
+    private void UpdateEnd(float perc)
+    {
+        _vignetteSettings.intensity = Mathf.Lerp(0f, 1f, perc);
+    }
+}
