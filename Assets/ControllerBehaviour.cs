c8815df9d5ae1dd07bfdb3f0c0edd3a3f5cd1504
Ivars Bergs <ivarsbergs@gmail.com>
2018-04-29 12:26:38 +0300

    meidzor progres

--- Assets/ControllerBehaviour.cs
+++ Assets/ControllerBehaviour.cs
@@ -98,7 +98,9 @@ public class ControllerBehaviour : MonoBehaviour
             hand.transform.parent = this.HandParent.transform;
             this.grabJoint.connectedBody = hand.GetComponent<HandBehaviour>().holdableRigidbody;
             this.liveHand = null;
-            SteamVR_Controller.Input(1).TriggerHapticPulse(3000);
+            SteamVR_Controller.Input(4).TriggerHapticPulse(3000);
+
+            //GetComponent<SteamVR_TrackedObject>().index.get
         }
     }
 }
