c2c60d479be81ab4a93a48b0bc2674745a733061
Sacristan <girts.kesteris@gmail.com>
2018-04-29 12:59:31 +0300

    add vignette fx

--- Assets/PostProcessingFXHandler.cs
+++ Assets/PostProcessingFXHandler.cs
@@ -9,21 +9,27 @@ public class PostProcessingFXHandler : MonoBehaviour
     [SerializeField] private float fadeTime = 1f;
 
     private PostProcessingBehaviour _postProcessingBehaviour;
+    PostProcessingProfile _postProcessingProfile;
     private VignetteModel.Settings _vignetteSettings;
 
     private void Start()
     {
         _postProcessingBehaviour = GetComponent<PostProcessingBehaviour>();
+        _postProcessingProfile = _postProcessingBehaviour.profile;
+        _vignetteSettings = _postProcessingProfile.vignette.settings;
+
         UpdateEnd(0f);
+        DoctorControl.Instance.OnPlayerStabbed += Instance_OnPlayerStabbed;
+    }
+
+    private void Instance_OnPlayerStabbed()
+    {
         StartCoroutine(EndRoutine());
     }
 
     private IEnumerator EndRoutine()
     {
         float t = 0f;
-
-        PostProcessingProfile postProcessingProfile = _postProcessingBehaviour.profile;
-        _vignetteSettings = postProcessingProfile.vignette.settings;
 
         while (t <= 1f)
         {
@@ -36,5 +42,6 @@ public class PostProcessingFXHandler : MonoBehaviour
     private void UpdateEnd(float perc)
     {
         _vignetteSettings.intensity = Mathf.Lerp(0f, 1f, perc);
+        _postProcessingProfile.vignette.settings = _vignetteSettings;
     }
 }
